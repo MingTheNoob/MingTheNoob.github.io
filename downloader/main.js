9480e4e5d91705144baf2c92606b11c4d94f0071
Ming <82824731+Mingzhi5@users.noreply.github.com>
2021-08-17 01:13:33 -0400

    Update downloader

--- downloader/main.js
+++ downloader/main.js
@@ -3,6 +3,6 @@ window.onload = function() {
         document.title = "Downloader - Failed";
         document.body.innerHTML = text;
     };
-    if (!window.location.search) error("Nothing to download");
+    if (!window.location.search) error("Nothing to download. You can just head back to https://ming.jp.net");
     else window.DownloadApi.convert(window.location.search, error);
 };
