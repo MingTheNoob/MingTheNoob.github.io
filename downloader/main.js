9eca2c81163cd6927ea700baa5db0d8e6ba677c7
Ming <82824731+Mingzhi5@users.noreply.github.com>
2021-09-17 11:02:14 -0400

    Update site

--- downloader/main.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-    const error = text => {
-        document.title = "Downloader - Failed";
-        document.body.innerHTML = text;
-    };
-    if (!window.location.search) error(`Nothing to download. You can just head back to <a href="https://ming.jp.net">ming.jp.net</a>`);
-    else window.DownloadApi.convert(window.location.search, error);
-};
